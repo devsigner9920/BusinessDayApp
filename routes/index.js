6b246e3cbc13a6dd524d28e64feb9066e6d3a933
WonJune Kang <devsigner9920@icloud.com>
2019-09-20 15:49:30 +0900

    update view!

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Business Day App', sub: 'Get Business Day for variety format!' });
 });
 
 router.get('/test', function (req, res, next) {
