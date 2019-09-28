da4e201b74ba43e8417067188bb97551ee118636
BuildTools <unconfigured@null.spigotmc.org>
2019-09-28 15:38:17 +0900

    Update db select logic

--- models/BizDayModel.js
+++ models/BizDayModel.js
@@ -1,14 +1,131 @@
 var mongoose = require('mongoose');
 
-var schema = new mongoose.Schema({
+var dateSchema = new mongoose.Schema({
     _year: String,
     months: [{
-            month: String,
-            days: [{
-                    day: String,
-                    isHoliday: String
-                }]
+        month: String,
+        days: [{
+            day: String,
+            isHoliday: String
         }]
+    }]
 });
 
-module.exports = mongoose.model("bizDay", schema, "bizDay");
+dateSchema.statics.findByYear = function (bizDate) {
+    let query = `
+        {
+          "$match": {
+            "_year": "` + bizDate['year'] +`",
+            "months": {
+              "$elemMatch": {
+                "days.isHoliday": "Y"
+              }
+            }
+          }
+        }`;
+    let projector = `
+        {
+          "$addFields": {
+            "months": {
+              "$filter": {
+                "input": {
+                  "$map": {
+                    "input": "$months",
+                    "as": "m",
+                    "in": {
+                      "month": "$$m.month",
+                      "days": {
+                        "$filter": {
+                          "input": "$$m.days",
+                          "as": "d",
+                          "cond": {
+                                "$eq": ["$$d.isHoliday", "Y"]
+                          }
+                        }
+                      }
+                    }
+                  }
+                },
+              "as": "m",
+              "cond": {
+                "$gt": [
+                  {
+                    "$size": "$$m.days"
+                  },
+                  0
+                ]
+                }
+              }
+            }
+          }
+        }`;
+    console.log('JSON.stringify(query) :', JSON.parse(query));
+    return this.aggregate([JSON.parse(query), JSON.parse(projector)]);
+}
+
+dateSchema.statics.findByMonth = function (bizDate) {
+    let query = `
+        {
+          "$match": {
+            "_year": "` + bizDate['year'] +`",
+            "months": {
+              "$elemMatch": {
+                "month": "` + bizDate['month'].zfill(2) + `",
+                "days.isHoliday": "Y"
+              }
+            }
+          }
+        }`;
+    let projector = `
+      {
+        "$addFields": {
+          "months": {
+            "$filter": {
+              "input": {
+                "$map": {
+                  "input": "$months",
+                  "as": "m",
+                  "in": {
+                    "month": "$$m.month",
+                    "days": {
+                      "$filter": {
+                        "input": "$$m.days",
+                        "as": "d",
+                        "cond": {
+                          "$eq": [
+                            "$$d.isHoliday",
+                            "Y"
+                          ]
+                        }
+                      }
+                    }
+                  }
+                }
+              },
+              "as": "m",
+              "cond": {
+                "$and": [
+                  { "$gt": [ {"$size": "$$m.days"}, 0 ] },
+                  { "$eq": [ "$$m.month", "` + bizDate['month'].zfill(2) + `" ] }
+                ]
+              }
+            }
+          }
+        }
+      }`;
+    return this.aggregate([JSON.parse(query), JSON.parse(projector)]);
+}
+
+/**
+ * Fill zero("0") until size equal to String size
+ * @param {*} size 
+ */
+String.prototype.zfill = function(size) {
+    let zs = this;
+    while(zs.length < (size || 2)) {
+        zs = "0" + zs;
+    }
+    return zs;
+}
+
+module.exports = mongoose.model("bizDay", dateSchema, "bizDay");
--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,41 @@ var BizDay = require('../models/BizDayModel');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Business Day App', sub: 'Get Business Day for variety format!'});
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Business Day App', sub: 'Get Business Day for variety format!' });
 });
 
 router.get('/json', function (req, res) {
-    var jsonData = {};
-    BizDay.find(function (err, data) {
-        if(err) return res.status(500).send({error: 'database failure'});
-        res.send(data);
-    });
+  var jsonData = {};
+
+  BizDay.find(function (err, data) {
+    if (err) return res.status(500).send({ error: 'database failure' });
+    res.json(data);
+  });
 });
 
 router.get('/bizDay/json', function (req, res) {
-  res.end();
+  if(req.query['term'] == 'Year') {
+    BizDay.findByYear(req.query)
+      .then((bizDay) => {
+        if(!bizDay) return res.status(404).send({ error: 'not found' });
+        res.json(bizDay);
+      }).catch((err) => {
+        res.status(500).send({ error: 'database failure' });
+      });
+  } else if(req.query['term'] == 'Month') {
+    BizDay.findByMonth(req.query)
+      .then((bizDay) => {
+        if(!bizDay) return res.status(404).send({ error: 'not found' });
+        res.json(bizDay);
+      }).catch((err) => {
+        res.status(500).send({ error: 'database failure' });
+      });
+  } else if(req.query['term'] == 'ETC') {
+
+  } else {
+    res.status(500).send({ error: 'Something Wrong...' });
+  }
 });
 
 router.get('/bizDay/xls', function (req, res) {
