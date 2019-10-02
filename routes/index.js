049e620183f4e6cfcfa666a39ab2cad17a03b75a
Chicpork <dkgkrmfo@gmail.com>
2019-10-02 22:50:15 +0900

    Add xml download

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var BizDay = require('../models/BizDayModel');
+var js2xmlparser = require("js2xmlparser");
 var router = express.Router();
 
 /* GET home page. */
@@ -14,6 +15,37 @@ router.get('/json', function (req, res) {
     if (err) return res.status(500).send({ error: 'database failure' });
     res.json(data);
   });
+});
+
+router.get('/bizDay/xml', function (req, res) {
+  res.set('Content-Type', 'text/xml');
+  if(req.query['term'] == 'Year') {
+    BizDay.findByYear(req.query)
+      .then((bizDay) => {
+        if(!bizDay) return res.status(404).send({ error: 'not found' });
+        res.send(js2xmlparser.parse("bizDays", bizDay[0]));
+      }).catch((err) => {
+        res.status(500).send({ error: 'database failure' });
+      });
+  } else if(req.query['term'] == 'Month') {
+    BizDay.findByMonth(req.query)
+      .then((bizDay) => {
+        if(!bizDay) return res.status(404).send({ error: 'not found' });
+        res.send(js2xmlparser.parse("bizDays", bizDay[0]));
+      }).catch((err) => {
+        res.status(500).send({ error: 'database failure' });
+      });
+  } else if(req.query['term'] == 'ETC') {
+    BizDay.findByBetweenDate(req.query)
+    .then((bizDay) => {
+      if(!bizDay) return res.status(404).send({ error: 'not found' });
+      res.send(js2xmlparser.parse("bizDays", bizDay));
+    }).catch((err) => {
+      res.status(500).send({ error: 'database failure' });
+    });
+  } else {
+    res.status(500).send({ error: 'Something Wrong...' });
+  }
 });
 
 router.get('/bizDay/json', function (req, res) {
