7c3402a09f0e9c6341e11e0517b0f91ac4d2cf82
Chicpork <dkgkrmfo@gmail.com>
2019-10-26 22:19:57 +0900

    Add api test url for kakao

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var BizDay = require('../models/BizDayModel');
 var js2xmlparser = require("js2xmlparser");
 var router = express.Router();
+var https = require('https');
+var proxy = require('express-http-proxy');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -85,5 +87,20 @@ router.get('/bizDay/xls', function (req, res) {
 router.get('/bizDay/csv', function (req, res) {
   res.end();
 });
+
+router.use('/api/kakao/book', proxy('dapi.kakao.com', {
+  proxyReqPathResolver: function (req) {
+    var parts = req.url.split('?');
+    var queryString = parts[1];
+    return '/v3/search/book' + (queryString ? '?' + queryString : '');
+  },
+  proxyReqOptDecorator: function (proxyReqOpts) {
+    proxyReqOpts.headers['Content-Type'] = 'application/json;charset=UTF-8';
+    proxyReqOpts.headers['Authorization'] = 'KakaoAK 2819d31d91b5f967b01f33aaef3810a2';
+    proxyReqOpts.method = 'GET';
+    return proxyReqOpts;
+  },
+  https: true
+}));
 
 module.exports = router;
